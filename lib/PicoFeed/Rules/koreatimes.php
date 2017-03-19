9186f2172bb25a0a93eab8d5fdebe65313d5c898
Colin W <git@part4.tech>
2017-03-18 22:28:24 -0500

    corrections and cleanup to previous push

--- lib/PicoFeed/Rules/koreatimes.php
+++ lib/PicoFeed/Rules/koreatimes.php
@@ -4,7 +4,7 @@ return array(
         '%.*%' => array(
             'test_url' => 'http://www.koreatimes.co.kr/www/news/nation/2015/12/116_192409.html',
             'body' => array(
-                '//div[@id="startts"]',
+                '//div[@id="p"]',
             ),
             'strip' => array(
                 '//div[@id="webtalks_btn_listenDiv"]',
--- lib/PicoFeed/Rules/the-ebook-reader.com.php
+++ lib/PicoFeed/Rules/the-ebook-reader.com.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
- 
->>>>>>> 17b3483bfc3092078d59e055c97b95d213c1a8c4
 return array(
     'grabber' => array(
         '%.*%' => array(
