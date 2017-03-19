d3c4e17baa992db5fc293b8154861a7ef923e6b7
Colin W <git@part4.tech>
2017-03-18 22:15:30 -0500

    change some rules; remove extra white space from 2nd lines to make rules uniform

--- lib/PicoFeed/Rules/koreatimes.php
+++ lib/PicoFeed/Rules/koreatimes.php
@@ -1,11 +1,10 @@
 <?php
-
 return array(
     'grabber' => array(
         '%.*%' => array(
             'test_url' => 'http://www.koreatimes.co.kr/www/news/nation/2015/12/116_192409.html',
             'body' => array(
-                '//div[@id="p"]',
+                '//div[@id="startts"]',
             ),
             'strip' => array(
                 '//div[@id="webtalks_btn_listenDiv"]',
--- lib/PicoFeed/Rules/the-ebook-reader.com.php
+++ lib/PicoFeed/Rules/the-ebook-reader.com.php
@@ -1,5 +1,8 @@
 <?php
+<<<<<<< HEAD
+=======
  
+>>>>>>> 17b3483bfc3092078d59e055c97b95d213c1a8c4
 return array(
     'grabber' => array(
         '%.*%' => array(
